7037aded95343a412418d63fbd521665b581280b
Hubert Kieruczenko <hub3rt65@gmail.com>
2023-04-08 16:18:24 +0200

    commit-4

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  authForm: FormGroup;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.authForm = new FormGroup({
+      username: new FormControl(['']),
+      password: new FormControl(['']),
+      passwordConfirmation: new FormControl([''])
+    });
+  }
 }
