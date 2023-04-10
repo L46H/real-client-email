06b506232d5a92e65895db1b9976755e81b9bcc9
Hubert Kieruczenko <hub3rt65@gmail.com>
2023-04-10 15:40:38 +0200

    commit-8

--- src/app/auth/validators/unique-username.ts
+++ src/app/auth/validators/unique-username.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AsyncValidator, FormControl } from '@angular/forms';
+import { map, catchError, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +14,28 @@ export class UniqueUsername implements AsyncValidator {
   validate = (formControl: FormControl) => {
     const { value } = formControl;
 
-    console.log(value);
-
-    return null;
-  }
+    // second argument is object that contains the data
+    // that we want to provide in the body of the post request
+    return this.http
+      .post<any>('https://api.angular-email.com/auth/username', {
+        username: value
+      })
+      .pipe(
+        map(value => {
+          if (value.available) {
+            return null;
+          }
+        }),
+        // no make difference if catchError
+        // operator is above or below that map operator
+        catchError(err => {
+          console.log(err);
+          if (err.error.username) {
+            return of({ nonUniqueUsername: true });
+          } else {
+            return of({ noConnection: true });
+          }
+        })
+      );
+  };
 }
