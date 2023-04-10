20fcce3eab5d526bb573d4e6d3916071fe8fc559
Hubert Kieruczenko <hub3rt65@gmail.com>
2023-04-10 16:05:48 +0200

    commit-9

--- src/app/auth/validators/unique-username.ts
+++ src/app/auth/validators/unique-username.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { AsyncValidator, FormControl } from '@angular/forms';
 import { map, catchError, of } from 'rxjs';
+import { AuthService } from '../auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UniqueUsername implements AsyncValidator {
-  constructor(private http: HttpClient) {}
+  constructor(private authService: AuthService) {}
 
   // in that case arrow function return context
   // instead of normal function which is not
@@ -16,26 +16,22 @@ export class UniqueUsername implements AsyncValidator {
 
     // second argument is object that contains the data
     // that we want to provide in the body of the post request
-    return this.http
-      .post<any>('https://api.angular-email.com/auth/username', {
-        username: value
+    return this.authService.usernameAvailable(value).pipe(
+      map(value => {
+        if (value.available) {
+          return null;
+        }
+      }),
+      // no make difference if catchError
+      // operator is above or below that map operator
+      catchError(err => {
+        console.log(err);
+        if (err.error.username) {
+          return of({ nonUniqueUsername: true });
+        } else {
+          return of({ noConnection: true });
+        }
       })
-      .pipe(
-        map(value => {
-          if (value.available) {
-            return null;
-          }
-        }),
-        // no make difference if catchError
-        // operator is above or below that map operator
-        catchError(err => {
-          console.log(err);
-          if (err.error.username) {
-            return of({ nonUniqueUsername: true });
-          } else {
-            return of({ noConnection: true });
-          }
-        })
-      );
+    );
   };
 }
