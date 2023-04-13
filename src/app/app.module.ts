5ed865dedb603da54c7ff8503b05df60c765b8b7
Hubert Kieruczenko <hub3rt65@gmail.com>
2023-04-13 11:12:06 +0200

    commit-20

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
+import { AuthHttpInterceptor } from './auth/auth-http-interceptor';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, AuthModule, HttpClientModule],
-  providers: [],
+  // we're essentially telling Angular dependency injection system
+  // that if anyone else asks for something of type HTTP_INTERCEPTORS
+  // then make sure we also give them a copy of the class
+  // useClass: AuthHttpInterceptor
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthHttpInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/auth/auth-http-interceptor.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent,
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthHttpInterceptor implements HttpInterceptor {
+  intercept(
+    // req obj will be send to remote server contains
+    // url, method, headers, params, etc..
+    req: HttpRequest<any>,
+    // the next interceptor that we need to run
+    // and eventually end on that function that
+    // we are going to run to make the request
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    console.log(req);
+    return next.handle(req);
+  }
+}
