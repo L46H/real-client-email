08d29c302628fbdf18e166bae59c6beeac7c121a
Hubert Kieruczenko <hub3rt65@gmail.com>
2023-04-17 12:46:18 +0200

    commit-33

--- src/app/inbox/email.service.ts
+++ src/app/inbox/email.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface EmailSummary {
+  id: string;
+  subject: string;
+  from: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
+  private rootUrl = 'https://api.angular-email.com';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getEmails() {
+    return this.http.get<EmailSummary[]>(`${this.rootUrl}/emails`, {
+      // include cookies
+      // in our case interceptors can handle that already
+      // withCredentials: true
+    })
+  }
 }
