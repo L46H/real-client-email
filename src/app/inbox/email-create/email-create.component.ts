96732a4319db003f19722a8dc3efbd97a9db5d9f
Hubert Kieruczenko <hub3rt65@gmail.com>
2023-04-20 13:04:17 +0200

    commit-45

--- src/app/inbox/email-create/email-create.component.ts
+++ src/app/inbox/email-create/email-create.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Email } from '../email';
 
 @Component({
   selector: 'app-email-create',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class EmailCreateComponent {
   showModal = false;
+  email: Email;
+
+  constructor() {
+    this.email = {
+      id: '',
+      to: '',
+      subject: '',
+      html: '',
+      text: '',
+      from: 'kier0@angular-email.com'
+    }
+  }
 }
--- src/app/inbox/email-form/email-form.component.ts
+++ src/app/inbox/email-form/email-form.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Email } from '../email';
 
 @Component({
   selector: 'app-email-form',
   templateUrl: './email-form.component.html',
   styleUrls: ['./email-form.component.css']
 })
-export class EmailFormComponent {
 
+export class EmailFormComponent implements OnInit {
+  emailForm: FormGroup;
+  @Input() email: Email;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    const { subject, from, to, text } = this.email;
+
+    this.emailForm = new FormGroup({
+      to: new FormControl(to),
+      from: new FormControl(from),
+      subject: new FormControl(subject),
+      text: new FormControl(text)
+    });
+  }
 }
