b15520e7bf95c19f1e23eeea020cea7850942a23
Hubert Kieruczenko <hub3rt65@gmail.com>
2023-04-21 18:23:00 +0200

    commit-53

--- src/app/inbox/email-reply/email-reply.component.ts
+++ src/app/inbox/email-reply/email-reply.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Email } from '../email';
 
 @Component({
@@ -6,9 +6,22 @@ import { Email } from '../email';
   templateUrl: './email-reply.component.html',
   styleUrls: ['./email-reply.component.css']
 })
-export class EmailReplyComponent {
-  showModal = false;
-  email: Email;
 
-  onSubmit(email: Email) {}
+export class EmailReplyComponent implements OnInit {
+  showModal = false;
+  @Input() email: Email;
+
+  ngOnInit(): void {
+    const text = this.email.text.replace(/\n/gi, '\n> ');
+
+    this.email = {
+      ...this.email,
+      from: this.email.to,
+      to: this.email.from,
+      subject: `RE: ${this.email.subject}`,
+      text: `\n\n\n----------- ${this.email.from} wrote:\n> ${text}`
+    };
+  }
+
+  onSubmit(email: Email): void {}
 }
